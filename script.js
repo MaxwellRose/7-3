f123623a0f68c0daad412bae3d7bdde4c632a633
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-24 16:14:53 +0000

    lots

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let userinput = 0
 document.getElementById('button').addEventListener('click', input)
 function input () {
   userinput = document.getElementById('num').value
-  if (userinput >= 17) {
-    alert('you can watch any movie')
-  } else if (userinput >= 13) {
-    alert('You can watch a pg 13 or a G movie')
-  } else if (userinput >= 5) {
-    alert('You can watch a G or PG movie')
-  } else {
-    alert('you are too young')
+  userinput = document.getElementById('day').value
+  if (userinput > 5 || userinput < 95) {
+    alert('You can get in for free any day')
+  } else if (userinput <= 13 && userinput >= 21) {
+    alert('You can get a students discount any day')
+  } else if (userinput > 13 && userinput < 21 || day === 'tuesday' || day === 'thursday') {
+    alert('you can go on a students discount')
   }
-}
+  else {
+    alert('You will need to pay full admission')
+  }
+} 

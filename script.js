f8e930caff4ecb1e10c2d2bcaa16321863a3dc26
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-24 16:29:00 +0000

    oof

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let userinput = 0
-let day = "monday"
 
 document.getElementById('button').addEventListener('click', input)
 function input () {
@@ -8,8 +7,8 @@ function input () {
     alert('You can get in for free any day')
   } else if (userinput <= 13 && userinput >= 21) {
     alert('You can get a students discount any day')
-  } else if (userinput > 13 && userinput < 21 || day === 'tuesday' || day === 'thursday') {
-    alert('you can go on a students discount')
+  } else if (userinput > 13 && userinput < 21 || const === 'tuesday' || const === 'thursday') {
+    alert { ('you can go on a students discount')
   }
   else {
     alert('You will need to pay full admission')
